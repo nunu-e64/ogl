57f21a7cac775d37d9ed643aae6093913d508190
Fumiya Amano <fumiya.amano@gree.co.jp>
2017-05-05 20:16:24 +0900

    tutorial1

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -11,61 +11,40 @@ using namespace glm;
 
 int main( void )
 {
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
-	}
+  if (!glfwInit()) {
+    fprintf(stderr, "GLFWの初期化に失敗しました。\n");
+    return -1;
+  }
 
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_SAMPLES, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);  // OpenGL3.3をつかいます。
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
+  GLFWwindow* window;
+  window = glfwCreateWindow(1024, 768, "Tutorial01", NULL, NULL);
+  if (window == nullptr) {
+    fprintf(stderr, "GLFWウィンドウのオープンに失敗しました。\n");
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glewExperimental = true;
+  if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "GLEWの初期化に失敗しました。\n");
+    return -1;
+  }
 
-	// Initialize GLEW
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+  do {
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    // バッファをスワップする
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && //ESCキーが押されたかをチェック
+           glfwWindowShouldClose(window) == 0); // ウィンドウが閉じたかをチェック
 
-	do{
-		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Draw nothing, see you in tutorial 2 !
-
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
-
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
-
-	return 0;
 }
-
